4c26f4fb7f039b587c27813ebd7cfd2bfc1a2ed6
aikooo7 <prozinhopro1973@gmail.com>
2023-09-22 23:24:39 +0100

    First working version.
    Signed-off-by: aikooo7 <prozinhopro1973@gmail.com>

---
+++ main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <ctype.h>
+
+int main() {
+  // Variables definition
+  char input[BUFSIZ];
+  char buffer[BUFSIZ];
+  unsigned char hashed[BUFSIZ];
+  size_t bytesRead;
+
+  EVP_MD_CTX *md_ctx;
+  const EVP_MD *md;
+  unsigned int md_len;
+
+  // Initialize OpenSSL library
+  OpenSSL_add_all_algorithms();
+
+  // Choose the message digest algorithm (SHA-512 in this case)
+  md = EVP_sha512();
+
+  // Create a message digest context
+  md_ctx = EVP_MD_CTX_new();
+
+  // Initialize the message digest context
+  EVP_DigestInit_ex(md_ctx, md, NULL);
+
+  // Prompt user for the file to be hashed
+  printf("What is the file you want to hash? ");
+
+  // Read user input
+  if (fgets(input, sizeof(input), stdin) == NULL) {
+    perror("input");
+    exit(-1);
+  }
+
+  // Remove newline character from input
+  input[strlen(input) - 1] = '\0';
+
+  const char *filename = input;
+
+  // Open the file for reading in binary mode
+  FILE *file = fopen(filename, "rb");
+
+  // Check if the file opened successfully
+  if (!file) {
+    perror("file");
+    exit(-1);
+  }
+
+  // Read and hash the file in chunks
+  while ((bytesRead = fread(buffer, 1, sizeof(buffer), file))) {
+    EVP_DigestUpdate(md_ctx, buffer, bytesRead);
+  }
+
+  // Finalize the hash computation
+  EVP_DigestFinal_ex(md_ctx, hashed, &md_len);
+
+  // Close the file
+  fclose(file);
+
+  // Open a file to append the hash result
+  file = fopen("../hashes.txt", "a");
+
+  // Write the filename and hash to the output file
+  fprintf(file, "Filename: %s | Hash: ", "hashes.txt");
+  printf("Your hash is: ");
+  for (int i = 0; i < md_len; i++) {
+    printf("%02x", hashed[i]);
+    fprintf(file, "%02x", hashed[i]);
+  }
+  printf("\n");
+  fprintf(file, "\n");
+
+  // Cleanup: Free resources
+  EVP_MD_CTX_free(md_ctx);
+  EVP_cleanup();
+
+  bool not_answered = true;
+
+  // Loop to ask if the user wants to hash another file
+  while (not_answered) {
+    printf("Want to hash another file? ");
+
+    // Read user input
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      perror("input");
+      exit(-1);
+    }
+
+    // Convert input to lowercase for case-insensitive comparison
+    for (int i = 0; i < strlen(input); i++) {
+      input[i] = tolower(input[i]);
+    }
+
+    // Remove newline character from input
+    input[strlen(input) - 1] = '\0';
+
+    if (strcmp("yes", input) == 0) {
+      // Clear the screen and start over
+      printf("\e[1;1H\e[2J");
+      main();
+      not_answered = false;
+    } else if (strcmp("no", input) == 0) {
+      printf("See you next time.\n");
+      not_answered = false;
+    } else {
+      printf("Incorrect input, write either yes or no.\n");
+    }
+  }
+
+  return 0;
+}
