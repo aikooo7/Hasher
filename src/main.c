ebcf263115ccc4ca58021e94c03b9494c202ae5e
aikooo7 <prozinhopro1973@gmail.com>
2023-12-20 10:58:56 +0000

    chore(main): remove unnecessary comments

--- src/main.c
+++ src/main.c
@@ -27,7 +27,6 @@
 #include <string.h>
 
 int main() {
-  // Variables definition
   char input[BUFSIZ];
   char buffer[BUFSIZ];
   unsigned char hashed[BUFSIZ];
@@ -38,51 +37,38 @@ int main() {
   const EVP_MD *md;
   unsigned int md_len;
 
-  // Initialize OpenSSL library
   OpenSSL_add_all_algorithms();
 
-  // Choose the message digest algorithm (SHA-512 in this case)
   md = EVP_sha512();
 
-  // Create a message digest context
   md_ctx = EVP_MD_CTX_new();
 
-  // Initialize the message digest context
   EVP_DigestInit_ex(md_ctx, md, NULL);
 
-  // Prompt user for the file to be hashed
   printf("What is the file you want to hash? ");
 
-  // Read user input
   if (fgets(filename, sizeof(filename), stdin) == NULL) {
     perror("filename");
     exit(-1);
   }
 
-  // Remove newline character from input
   filename[strlen(filename) - 1] = '\0';
 
-  // Open the file for reading in binary mode
   FILE *file = fopen(filename, "rb");
 
-  // Check if the file opened successfully
   if (!file) {
     perror("file");
     exit(-1);
   }
 
-  // Read and hash the file in chunks
   while ((bytesRead = fread(buffer, 1, sizeof(buffer), file))) {
     EVP_DigestUpdate(md_ctx, buffer, bytesRead);
   }
 
-  // Finalize the hash computation
   EVP_DigestFinal_ex(md_ctx, hashed, &md_len);
 
-  // Close the file
   fclose(file);
 
-  // Prints the hash
   printf("Your hash is: ");
   for (int i = 0; i < md_len; i++) {
     printf("%02x", hashed[i]);
@@ -90,22 +76,19 @@ int main() {
   printf("\n");
 
   bool not_answered = true;
-  // Loop to ask the user if he wants to write the hash in the file.
+
   while (not_answered) {
     printf("Want to write the hash to a file? ");
 
-    // Read user input
     if (fgets(input, sizeof(input), stdin) == NULL) {
       perror("input");
       exit(-1);
     }
 
-    // Convert input to lowercase for case-insensitive comparison
     for (int i = 0; i < strlen(input); i++) {
       input[i] = tolower(input[i]);
     }
 
-    // Remove newline character from input
     input[strlen(input) - 1] = '\0';
 
     if (strcmp("yes", input) == 0) {
@@ -126,34 +109,26 @@ int main() {
     }
   }
 
-  // Write the filename and hash to the output file
-
-  // Cleanup: Free resources
   EVP_MD_CTX_free(md_ctx);
   EVP_cleanup();
 
   not_answered = true;
 
-  // Loop to ask if the user wants to hash another file
   while (not_answered) {
     printf("Want to hash another file? ");
 
-    // Read user input
     if (fgets(input, sizeof(input), stdin) == NULL) {
       perror("input");
       exit(-1);
     }
 
-    // Convert input to lowercase for case-insensitive comparison
     for (int i = 0; i < strlen(input); i++) {
       input[i] = tolower(input[i]);
     }
 
-    // Remove newline character from input
     input[strlen(input) - 1] = '\0';
 
     if (strcmp("yes", input) == 0) {
-      // Clear the screen and start over
       printf("\e[1;1H\e[2J");
       main();
       not_answered = false;
